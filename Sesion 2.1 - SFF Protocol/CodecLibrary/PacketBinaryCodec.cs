a4795cbe14ce6ad2d4313ff7199cd57bfdb0edad
PabloTerroba <uo289073@uniovi.es>
2025-03-01 10:46:11 +0100

    Solución con un proyecto
    Solución solamente con biblioteca de clases de Codecs (Encode, Decode...)

---
+++ Sesion 2.1 - SFF Protocol/CodecLibrary/PacketBinaryCodec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CodecLibrary
+{
+    class PacketBinaryCodec : BinaryCodec<Packet>
+    {
+        public override void WriteBinaryData(BinaryWriter writer, Packet message)
+        {
+            writer.Write((int)message._type);
+            writer.Write(message._bodyLength);
+            writer.Write(message._body);
+        }
+
+        public override Packet ReadBinaryData(BinaryReader reader)
+        {
+            int typeRaw = reader.ReadInt32();
+            int bodyLength = reader.ReadInt32();
+
+            byte[] body = new byte[bodyLength];
+            reader.Read(body, 0, bodyLength);
+
+            return new Packet((PacketBodyType)typeRaw, bodyLength, body);
+        }
+    }
+}
